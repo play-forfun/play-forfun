0ed669b59c43fe08d15a70b524f3d9127b3e9644
zyy_y <zhyang_ang@163.com>
2020-02-27 12:01:39 +0800

    引入了element-ui

--- src/views/Find.vue
+++ src/views/Find.vue
@@ -2,12 +2,24 @@
   <div>
     <h1>发现</h1>
     <p>这是发现的内容，哈哈哈</p>
+    <el-button type="primary" @click="myClick">引入了ELEMENT-UI点击</el-button>
   </div>
 </template>
 
 <script>
   export default {
-    name: "Find"
+    name: "Find",
+    mounted() {
+
+    },
+    methods:{
+      myClick(){
+        this.$message({
+          message: '恭喜你，这是一条成功消息',
+          type: 'success'
+        });
+      }
+    }
   }
 </script>
 
