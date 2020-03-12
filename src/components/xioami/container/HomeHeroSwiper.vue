214d1ad85312a15689812ae7f8707abbdc9e992a
Roger-mrj <645106533@qq.com>
2020-03-12 18:04:55 +0800

    roger

--- src/components/xioami/container/HomeHeroSwiper.vue
+++ src/components/xioami/container/HomeHeroSwiper.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-hero-swiper">
     <div class="block">
-      <el-carousel trigger="click" height="460px" arrow="hover" indicator-position="right" interval="10000000">
+      <el-carousel trigger="click" height="460px" arrow="always" interval="10000000">
         <el-carousel-item v-for="item in swiperImgs" :key="item">
           <img :src="item.url" alt="">
         </el-carousel-item>
@@ -47,16 +47,19 @@
     margin: 0;
     padding: 0;
   }
-  /*.el-calendar .el-carousel__indicator */
+  /*直接在网页中F12找到对应的类，在这里重写即可，设置宽高一致，圆角50%则显示为圆形*/
   .el-carousel__button{
     width: 10px;
     height: 10px;
     border-radius:50%;
+    opacity:.8;
+    border:1px solid #888888;
+    background-color: #393939;
   }
-  .el-carousel__indicators{
-/*    display: flex;
-    justify-content: right;*/
-    display: block;
-    float: right;
+  .el-carousel__indicators {
+    /*    display: flex;
+        justify-content: flex-end;
+        display: block;
+        float: right;*/
   }
 </style>
